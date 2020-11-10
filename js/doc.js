55a0768c600756014ec275dabf32ae98c2b3e6e2
Hrithik <hrithikmallick2000@gmail.com>
2020-11-10 19:43:51 +0530

    first commit

---
+++ js/doc.js
@@ -0,0 +1,18 @@
+console.log("js work fine");
+ let ab =document.getElementById('secBTn');
+  ab.addEventListener("click",()=>{
+    let a = document.getElementById("hid-1").style.display="none";
+    let b = document.getElementById("hid-2").style.display="block";
+    let c = document.getElementById("hid-3").style.display="none";
+       })
+  
+
+let firstBTn =document.getElementById('firstBtn');
+firstBtn.addEventListener("click",()=>{
+let x = document.getElementById('hid-1').style.display="block";
+let y = document.getElementById('hid-2').style.display="none";
+let z = document.getElementById('hid-3').style.display="none";
+})
+
+
+       
